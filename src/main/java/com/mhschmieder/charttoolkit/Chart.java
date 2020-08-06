1d7bec9b722d3d27f6083247c271528ae58d33fa
Mark Schmieder <markhschmieder@gmail.com>
2020-08-05 23:01:06 -0700

    Update Chart.java
    Lots of work to start fleshing out the common code that is roughly the same (or can be easily paramterized to be the same algorithmically) for both polar charts and Cartesian charts.

--- src/main/java/com/mhschmieder/charttoolkit/Chart.java
+++ src/main/java/com/mhschmieder/charttoolkit/Chart.java
@@ -1,10 +1,13 @@
 package com.mhschmieder.charttoolkit;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Insets;
 
+import com.mhschmieder.graphicstoolkit.font.FontUtilities;
 import com.mhschmieder.guitoolkit.component.VectorizationXPanel;
 
 /**
@@ -43,6 +46,8 @@ public abstract class Chart extends VectorizationXPanel {
      * application (and platforms) and the sharpest text.
      *
      * @return A font that is appropriate for chart titles; bold and large
+     *
+     * @version 1.0
      */
     @SuppressWarnings("nls")
     public final static Font makeTitleFontCandidate() {
@@ -59,6 +64,39 @@ public abstract class Chart extends VectorizationXPanel {
     protected String      chartTitle;
 
     /**
+     * The insets of the chart from the host panel on all four sides.
+     */
+    protected Insets      chartInsets;
+
+    /**
+     * This variable contains the size of the chart area in <i>pixels</i>. The
+     * chart area is usually offset from upper left-hand corner of the host
+     * panel, and also accounts for the chart title (if present), so variables
+     * {@code ulx} and {@code uly} specify the origin of the chart area.
+     */
+    protected Dimension   chartSize;
+
+    /**
+     * The x value for the upper left corner of the chart area in pixels.
+     */
+    protected int         ulx;
+
+    /**
+     * The y value for the upper left corner of the chart area in pixels.
+     */
+    protected int         uly;
+
+    /**
+     * The x value for the lower right corner of the chart area in pixels.
+     */
+    protected int         lrx;
+
+    /**
+     * The y value for the lower right corner of the chart area in pixels.
+     */
+    protected int         lry;
+
+    /**
      * The {@link Color} to use for the Grid Lines.
      */
     protected Color       gridColor;
@@ -67,6 +105,16 @@ public abstract class Chart extends VectorizationXPanel {
      * The {@link FontMetrics} for the currently active title {@link Font}.
      */
     protected FontMetrics titleFontMetrics;
+
+    /**
+     * The power of ten by which the x-axis tic labels should be multiplied.
+     */
+    protected int         xExp;
+
+    /**
+     * The power of ten by which the x-axis tic labels should be multiplied.
+     */
+    protected int         yExp;
 
     //////////////////////////// Constructors ////////////////////////////////
 
@@ -89,16 +137,88 @@ public abstract class Chart extends VectorizationXPanel {
         // Pass in the title as a constructor argument?
         chartTitle = "";
 
+        // Assume that axis labels are on the left and bottom, meaning we want
+        // slightly asymmetrical insets for the chart from its host panel.
+        chartInsets = new Insets( 10, 16, 16, 10 );
+
+        // Start with an initial chart size that is guaranteed to show something
+        // even if the window has been significantly shrunk.
+        chartSize = new Dimension( 100, 100 );
+
+        // Set the initial chart area to a reasonable size that fits in minimal
+        // windows, just in case the derived classes don't cleanly initialize
+        // things in an ideal order.
+        ulx = 1;
+        uly = 1;
+        lrx = 100;
+        lry = 100;
+
         // Default to a subtle monochrome gray that shows against most
         // backgrounds without getting masked.
         gridColor = Color.LIGHT_GRAY;
 
-        // make sure the Title Font Metrics aren't null at startup time.
+        // Make sure the Title Font Metrics aren't null at startup time.
         final Font titleFontCandidate = Chart.makeTitleFontCandidate();
         titleFontMetrics = getFontMetrics( titleFontCandidate );
+
+        // Start with the assumption that exponents are not needed for either
+        // axis; this is only required for huge magnitude numbers that would
+        // otherwise cause tic labels to overlap and become unreadable.
+        xExp = 0;
+        yExp = 0;
     }
 
     ////////////////// Accessor methods for private data /////////////////////
+
+    /**
+     * Returns the x value for the upper left corner of the chart area in
+     * pixels.
+     *
+     * @return The x value for the upper left corner of the chart area in pixels
+     *
+     * @version 1.0
+     */
+    public final int getUlx() {
+        return ulx;
+    }
+
+    /**
+     * Returns the y value for the upper left corner of the chart area in
+     * pixels.
+     *
+     * @return The y value for the upper left corner of the chart area in pixels
+     *
+     * @version 1.0
+     */
+    public final int getUly() {
+        return uly;
+    }
+
+    /**
+     * Returns the x value for the lower right corner of the chart area in
+     * pixels.
+     *
+     * @return The x value for the lower right corner of the chart area in
+     *         pixels
+     *
+     * @version 1.0
+     */
+    protected final int getLrx() {
+        return lrx;
+    }
+
+    /**
+     * Returns the y value for the lower right corner of the chart area in
+     * pixels.
+     *
+     * @return The y value for the lower right corner of the chart area in
+     *         pixels
+     *
+     * @version 1.0
+     */
+    protected final int getLry() {
+        return lry;
+    }
 
     /**
      * Returns the chart title, or an empty string if there is none.
@@ -172,17 +292,56 @@ public abstract class Chart extends VectorizationXPanel {
     public abstract void drawChart( final Graphics2D graphicsContext );
 
     /**
-     * Draws the chart title, generally above the chart or chart group.
-     * <p>
-     * This method is abstract as the base class has no data or chart components
-     * and is just present for enforcing basic shared chart behavior.
+     * Draws the chart title, generally above a single chart or group of charts.
      *
      * @param graphicsContext
      *            The graphics context.
      *
      * @version 1.0
      */
-    public abstract void drawChartTitle( final Graphics2D graphicsContext );
+    public final void drawChartTitle( final Graphics2D graphicsContext ) {
+        // There is nothing to do if the chart title isn't present.
+        if ( ( chartTitle == null ) || chartTitle.trim().isEmpty() ) {
+            return;
+        }
+
+        // Cache the current Color and Font to restore later.
+        final Color color = graphicsContext.getColor();
+        final Font font = graphicsContext.getFont();
+
+        final int maxCharacterHeight = uly - 15;
+
+        // Get the maximum string width for determining Font Metrics.
+        final int maxStringWidth = ( int ) Math.round( 0.25d * ( 3d * chartSize.width ) );
+
+        // Use the standard foreground color with the Title Font.
+        graphicsContext.setColor( getForeground() );
+        final Font titleFontCandidate = Chart.makeTitleFontCandidate();
+        titleFontMetrics = FontUtilities.pickFont( graphicsContext,
+                                                   titleFontCandidate,
+                                                   Chart.DEFAULT_TITLE_FONT_SIZE,
+                                                   maxCharacterHeight,
+                                                   maxStringWidth,
+                                                   chartTitle );
+
+        // Center the Title over the charting region, not over the window.
+        final int stringWidth = titleFontMetrics.stringWidth( chartTitle );
+        final int titleX = ulx + ( int ) Math.round( 0.5d * ( chartSize.width - stringWidth ) );
+
+        // Vertical space for title, including the top padding above the title.
+        //
+        // We assume a one-line title, and the title above the main chart.
+        final int stringHeight = titleFontMetrics.getHeight();
+        final int titleY = ( yExp == 0 )
+            ? ( int ) Math.round( 0.5d * stringHeight ) + 2 + chartInsets.top
+            : chartInsets.top;
+
+        graphicsContext.drawString( chartTitle, titleX, titleY );
+
+        // Restore the previous Color and Font.
+        graphicsContext.setColor( color );
+        graphicsContext.setFont( font );
+    }
 
     //////////////////////// XPanel method overrides /////////////////////////
 
